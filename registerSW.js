79942ae16df19958236dac1052a3e89e12afe12b
eitanisrael <eitanisrael461@gmail.com>
2026-07-19 20:20:07 +0300

    Unify site at root: app, operators guide, SEO files

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Tweetelligent/sw.js', { scope: '/Tweetelligent/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
